16fa3cbd48e678525e709c2512425044caac2743
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-14 15:20:37 +0200

    Write custom routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
-import express from 'express';
+import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
 
-const app = express();
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    console.log('GET /');
+    console.log('GET /')
     res.status(200)
     res.json({ message: 'Hello World!' })
-    res.end();
+    res.end()
 })
 
-export default app;
+app.use('/api', router)
+
+export default app
