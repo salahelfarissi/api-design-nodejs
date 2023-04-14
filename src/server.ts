455b2d43399cb2ad372cf4b264eb58415047e992
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-14 15:46:48 +0200

    Import auth

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,23 @@
-import express from 'express'
-import router from './router'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express"
+import router from "./router"
+import morgan from "morgan"
+import cors from "cors"
+import { protect } from "./modules/auth"
 
 const app = express()
 
 app.use(cors())
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    console.log('GET /')
-    res.status(200)
-    res.json({ message: 'Hello World!' })
-    res.end()
+app.get("/", (req, res) => {
+  console.log("GET /")
+  res.status(200)
+  res.json({ message: "Hello World!" })
+  res.end()
 })
 
-app.use('/api', router)
+app.use("/api", protect, router)
 
 export default app
