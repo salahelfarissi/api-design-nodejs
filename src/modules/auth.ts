22b08da199337dbe694e16d63e61d37d13b37974
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-15 23:40:03 +0200

    Add auth protection

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -6,7 +6,7 @@ export const comparePasswords = (password, hash) => {
 }
 
 export const hashPassword = (password) => {
-  return bcrypt.hash(password, 5)
+  return bcrypt.hash(password, 3)
 }
 
 export const createJWT = (user) => {
