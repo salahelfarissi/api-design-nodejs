c2504f7a9cbb35ea516fb91715f7e1c149607fca
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-15 23:39:18 +0200

    Add user and sign in routes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import router from "./router"
 import morgan from "morgan"
 import cors from "cors"
 import { protect } from "./modules/auth"
+import { createNewUser, signin } from "./handlers/user"
 
 const app = express()
 
@@ -19,5 +20,8 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api", protect, router)
+
+app.post("/user", createNewUser)
+app.post("/signin", signin)
 
 export default app
