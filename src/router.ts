34324f587357e5856c6cf62a79b43f50293c33ae
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-16 09:23:27 +0200

    Do input validation for put product id

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express'
+import { Router } from "express"
+import { body, validationResult } from "express-validator"
 
 const router = Router()
 
@@ -6,32 +7,39 @@ const router = Router()
  * Product
  */
 
-router.get('/product', (req, res) => {
-    res.json({ message: 'Hello World!' })
+router.get("/product", (req, res) => {
+  res.json({ message: "Hello World!" })
 })
-router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
-router.post('/product', () => {})
-router.delete('/product/:id', () => {})
+router.get("/product/:id", () => {})
+router.put("/product/:id", body("name").isString(), (req, res) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  res.json({ message: "Hello World!" })
+})
+router.post("/product", () => {})
+router.delete("/product/:id", () => {})
 
 /**
  * Update
  */
 
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
-router.delete('/update/:id', () => {})
+router.get("/update", () => {})
+router.get("/update/:id", () => {})
+router.put("/update/:id", () => {})
+router.post("/update", () => {})
+router.delete("/update/:id", () => {})
 
 /**
  * Update Point
  */
 
-router.get('/updatepoint', () => {})
-router.get('/updatepoint/:id', () => {})
-router.put('/updatepoint/:id', () => {})
-router.post('/updatepoint', () => {})
-router.delete('/updatepoint/:id', () => {})
+router.get("/updatepoint", () => {})
+router.get("/updatepoint/:id", () => {})
+router.put("/updatepoint/:id", () => {})
+router.post("/updatepoint", () => {})
+router.delete("/updatepoint/:id", () => {})
 
 export default router
