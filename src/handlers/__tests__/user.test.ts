2b5e87e6dcce5c6c7c242a5373e82bfdce820882
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-05-07 11:53:25 +0200

    Test create new user

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,13 @@
+import * as user from "../user"
+
 describe("user handler", () => {
-  it("should create a new user", () => {
-    expect(true).toBe(true)
+  it("should create a new user", async () => {
+    const req = { body: { username: "test", password: "test" } }
+    const res = {
+      json({ token }) {
+        expect(token).toBeTruthy()
+      },
+    }
+    await user.createNewUser(req, res, () => {})
   })
 })
